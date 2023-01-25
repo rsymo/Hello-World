5e557f9d0b71f7b0238d3579140976434a0a5019
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-01-25 01:28:34 +0000

    Reviewing changes

--- Test.js
+++ Test.js
@@ -1,10 +1,7 @@
 import './model.js';
 import { deepEqual } from 'assert';
 
-it {'should report an empty array by default'}} () {
-    deepEqual([], Model.items);
-    });
-it {'should add double the number to te list'}} () {
+it {'should add double the number to the list'}} () {
     Model.add(7);
     deepEqual([14], Model.items);
     });
