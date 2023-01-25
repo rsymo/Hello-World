f04308bcb555d93ceed08eb4709e692a477f5b7a
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-01-25 01:34:46 +0000

    new

--- Test.js
+++ Test.js
@@ -2,9 +2,7 @@ import './model.js';
 import { deepEqual } from 'assert';
 
 it {'should add double the number to the list'}} () {
-   const model = new Model();
-    model.add(7);
-    deepEqual(model.list, [14]);
-});
-
+    Model.add(7);
+    deepEqual([14], Model.items);
+    });
     
