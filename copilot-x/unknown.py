638438a10273090b52fe80945bd52b544adffd09
Roger Symonds <rsymo@github.com>
2023-08-18 14:21:01 +1000

    20230818

--- copilot-x/unknown.py
+++ copilot-x/unknown.py
@@ -4,13 +4,3 @@ p_regex = r'^\+?1?\d{9,15}$'
 s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$'
 
 
-def check_validity(text, regex):
-    if re.search(regex, text):
-        return True
-    else:
-        return False
-
-if __name__ == '__main__':
-    print(("valid", "invalid")[check_validity('', email_regex)])
-    print(("valid", "invalid")[check_validity('', phone_regex)]) 
-    print(("valid", "invalid")[check_validity('', password_regex)])
